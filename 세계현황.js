1a6f6bf60b440c965155c7ab2b04f633c77c354d
knucoronaCrawler <chanhuiseok@gmail.com>
2020-09-17 08:02:27 +0000

    Auto - Update data with Crawling

--- 세계현황.js
+++ 세계현황.js
@@ -1,6 +1,6 @@
 ﻿var 세계현황 = [
-    30042776,
-    945166,
-    21808582,
-    "Last updated: September 17, 2020, 06:49 GMT"
+    30049278,
+    945320,
+    21814429,
+    "Last updated: September 17, 2020, 07:54 GMT"
 ];
