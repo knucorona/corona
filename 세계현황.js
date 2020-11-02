35a16e8119a0626257937cd3108d7e54205ef0bc
unknown <chanhuicom@gmail.com>
2020-11-02 23:17:43 +0900

    크롤러 수동 실행(임시)

--- 세계현황.js
+++ 세계현황.js
@@ -1,6 +1,6 @@
 ﻿var 세계현황 = [
-    46966981,
-    1207081,
+    46969213,
+    1207084,
     33852536,
-    "Last updated: November 02, 2020, 14:01 GMT"
+    "Last updated: November 02, 2020, 14:15 GMT"
 ];
