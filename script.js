39823247c6d2d9af25de4bb0d4cc529b76a78b86
Chanhui Seok <chanhuicom@gmail.com>
2022-01-23 10:29:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ doc.getElementById("Now").innerHTML = updateString;
   doc.getElementById("세계사망자").innerHTML = String(세계현황[1].toLocaleString());
   doc.getElementById("세계완치자").innerHTML = String(세계현황[2].toLocaleString());
 
-  doc.getElementById("update3").innerHTML = '<br><i class="fas fa-sync-alt"></i> '+ String(국내현황[6]) +' (차이 : 전일 00시 대비)';
+  doc.getElementById("update3").innerHTML = '<br><i class="fas fa-sync-alt"></i> '+ '업데이트 일시 중지';
 
   // 세계순위현황 업데이트시간 텍스트
   doc.getElementById("W_updateDate").innerHTML = updateString;
